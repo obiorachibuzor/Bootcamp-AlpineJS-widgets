d5e5b93a2b1567a6abe8fbf918344562affb0efa
Chibuzor Obiora <218098562@student.uj.ac.za>
2022-07-08 15:40:39 +0200

    Create word-game-widget.js

---
+++ word-game-widget.js
@@ -0,0 +1,33 @@
+
+    Alpine.data('wordGameWidget', function(){
+        return {
+            theSentence: '',
+       longestMessage: '',
+       longW(){
+     this.longestMessage = longestWord(this.theSentence);
+     setTimeout(() => {
+        this.longestMessage = '';
+        this.theSentence = '';
+        }, 9000);
+     },
+     shortestMessage: '',
+     shortW(){
+        this.shortestMessage = shortestWord(this.theSentence);
+        setTimeout(() => {
+           this.shortestMessage = '';
+           this.theSentence = '';
+           }, 9000);
+        },
+
+        sentenceCount: '',
+        wordCount(){
+            this.sentenceCount = wordLengths(this.theSentence);
+            setTimeout(() => {
+               this.sentenceCount = '';
+               this.theSentence = '';
+               }, 9000);
+            },
+        }
+    })
+
+})
